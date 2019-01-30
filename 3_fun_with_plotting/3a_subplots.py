fefea28f2dd793cfe283aab47cf27ed846e0c57e
alexjsolar <alexanderjames@mssl7l.mssl.ucl.ac.uk>
2019-01-30 14:00:53 +0000

    Added support for 'current working directory'

--- 3_fun_with_plotting/3a_subplots.py
+++ 3_fun_with_plotting/3a_subplots.py
@@ -1,21 +1,25 @@
+#-----------------------------------------------------------------------------#
+#------------------------ How to Plot Multiple Images ------------------------#
+#------------------------------ Alexander James ------------------------------#
+#-----------------------------------------------------------------------------#
 
+#---- Imports ----------------------------------------------------------------#
 
-#---- Imports ----------------------------------------------------------#
-
+import os                                   #For getting working directory
 from os import listdir                      #For finding FITS files
 from os.path import isfile, join            #For finding FITS files
 import sunpy.map                            #For making maps
 import matplotlib.pyplot as plt             #For plotting
 
-#---- Inputs -----------------------------------------------------------#
+#---- Inputs -----------------------------------------------------------------#
 
-data_path = ('/Users/alexanderjames/Documents/PhD/Python_Code'
-             '/Python_Taster/3_fun_with_plotting/')
+cwd = os.getcwd()                           #Get current working directory
+cwd = cwd + '/3_fun_with_plotting/'         #Specify current working directory
 
-#---- Read Data --------------------------------------------------------#
+#---- Read Data --------------------------------------------------------------#
 
 #Find all FITS files in the data_path
-files = [data_path+f for f in listdir(data_path) if isfile(join(data_path, f))
+files = [cwd+f for f in listdir(cwd) if isfile(join(cwd, f))
          and '.fits' in f]
 files = sorted(files)                       #sort to get them in date-order
 
@@ -23,7 +27,7 @@ smap1 = sunpy.map.Map(files[0])             #Make first map
 
 smap2 = sunpy.map.Map(files[1])             #Make second map
 
-#---- Plotting ---------------------------------------------------------#
+#---- Plotting ---------------------------------------------------------------#
 
 fig = plt.figure(figsize=(10,4))         #Set up a blank Figure. Size in inches
 
@@ -35,5 +39,5 @@ ax2 = fig.add_subplot(1,2,2, projection=smap2)  #1 row, 2 columns, 2nd axis
 smap2.plot()                                    #Plot the second Map on axis 2
 ax2.grid(False)                                 #Turn off coordinate grid
 
-plt.savefig('subplots.png')                     #Save the Figure
+plt.savefig(cwd+'subplots.png')                     #Save the Figure
 plt.show()                                      #Show the Figure
--- 3_fun_with_plotting/3b_cropping.py
+++ 3_fun_with_plotting/3b_cropping.py
@@ -1,7 +1,11 @@
+#-----------------------------------------------------------------------------#
+#-------------------- How to Crop a Submap from an Image ---------------------#
+#------------------------------ Alexander James ------------------------------#
+#-----------------------------------------------------------------------------#
 
+#---- Imports ----------------------------------------------------------------#
 
-#---- Imports ----------------------------------------------------------#
-
+import os                                   #for getting working directory
 from os import listdir                      #for finding FITS files
 from os.path import isfile, join            #for finding FITS files
 import sunpy.map                            #for making maps
@@ -11,27 +15,26 @@ import astropy.units as u                   #for giving arcseconds
 from astropy.coordinates import SkyCoord    #for coordinates
 from matplotlib import patches              #for drawing boxes
 
-#---- Inputs -----------------------------------------------------------#
+#---- Inputs -----------------------------------------------------------------#
 
-data_path = ('/Users/alexanderjames/Documents/PhD/Python_Code'
-             '/Python_Taster/3_fun_with_plotting/')
+cwd = os.getcwd()                           #Get current working directory
+cwd = cwd + '/3_fun_with_plotting/'         #Specify current working directory
 
 # Define a region of interest
-x0 = -300 * u.arcsec                    #centre x-coordinate
-y0 = -300 * u.arcsec                    #centre y-coordinate
-length = 250 * u.arcsec                 #length of box
-height = 250 * u.arcsec                 #height of box
+x0 = -300 * u.arcsec                        #Centre x-coordinate
+y0 = -300 * u.arcsec                        #Centre y-coordinate
+length = 250 * u.arcsec                     #Length of box
+height = 250 * u.arcsec                     #Height of box
 
-#---- Read Data --------------------------------------------------------#
+#---- Read Data --------------------------------------------------------------#
 
 #Find all FITS files in the data_path
-files = [data_path+f for f in listdir(data_path) if isfile(join(data_path, f))
-         and '.fits' in f]
+files = [cwd+f for f in listdir(cwd) if isfile(join(cwd, f)) and '.fits' in f]
 files = sorted(files) #sort alphabetically to get them in date-order
 
 smap1 = sunpy.map.Map(files[0]) #Make first map
 
-#---- Cropping ---------------------------------------------------------#
+#---- Cropping ---------------------------------------------------------------#
 
 #Create coordinates of submap corners
 bottom_left = SkyCoord(x0 - length, y0 - length, frame=smap1.coordinate_frame)
@@ -40,7 +43,7 @@ top_right = SkyCoord(x0 + length, y0 + length, frame=smap1.coordinate_frame)
 #Create submap
 smap2 = smap1.submap(bottom_left, top_right)
 
-#---- Plotting ---------------------------------------------------------#
+#---- Plotting ---------------------------------------------------------------#
 
 fig = plt.figure(figsize=(10,4))                #Set up a blank Figure
 
@@ -48,11 +51,11 @@ ax1 = fig.add_subplot(1,2,1, projection=smap1)  #1 row, 2 columns, 1st axis
 smap1.plot()                                    #Plot the first Map on axis 1
 ax1.grid(False)                                 #Turn off coordinate grid
 #Draw a box of the submap area on the full image
-smap1.draw_rectangle(bottom_left, length*2, height*2) 
+smap1.draw_rectangle(bottom_left, length*2, height*2)
 
 ax2 = fig.add_subplot(1,2,2, projection=smap2)  #1 row, 2 columns, 2nd axis
 smap2.plot()                                    #Plot the second Map on axis 2
 ax2.grid(False)                                 #Turn off coordinate grid
 
-plt.savefig('cropping.png')                     #Save the Figure
+plt.savefig(cwd+'cropping.png')                     #Save the Figure
 plt.show()                                      #Show the Figure
