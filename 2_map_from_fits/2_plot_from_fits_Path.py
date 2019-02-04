ea0ad1692deb3d1e8f390f23ee672be946127243
alexjsolar <alexanderjames@mssl7l.mssl.ucl.ac.uk>
2019-02-04 10:08:11 +0000

    Formatted for readability and consistency

--- 2_map_from_fits/2_plot_from_fits_Path.py
+++ 2_map_from_fits/2_plot_from_fits_Path.py
@@ -1,29 +1,34 @@
 #-----------------------------------------------------------------------------#
 #------------------- How to Plot an Image from a FITS File -------------------#
+#------------------------------ Diego De Pablos ------------------------------#
 #------------------------------ Alexander James ------------------------------#
 #-----------------------------------------------------------------------------#
 
 #---- Imports ----------------------------------------------------------------#
-from pathlib import Path                # Path Operations
-import sunpy.map                        # Mapping
-import matplotlib.pyplot as plt         # Plotting
+
+from pathlib import Path                    #For path operations
+import sunpy.map                            #For making maps
+import matplotlib.pyplot as plt             #For plotting
 
 #---- Inputs -----------------------------------------------------------------#
-work_dir = Path.cwd() /'2_map_from_fits'     # Formatting Paths is easy
+
+work_dir = Path.cwd() /'2_map_from_fits'    #Formatting Paths is easy
+
 #---- Read Data --------------------------------------------------------------#
 
-filelist = sorted(work_dir.glob('*.fits')) # Selects all files with fits suffix
+filelist = sorted(work_dir.glob('*.fits'))  #Selects all files with fits suffix
 
 # Each file has a corresponding index, sort normally, look for .fits extensions
 for index, file in enumerate(filelist):
 
-    smap = sunpy.map.Map(str(file))          #Make a map from the file
-    #---- Plotting ---------------------------------------------------------------#
+    smap = sunpy.map.Map(str(file))         #Make a map from the file
+
+    #---- Plotting -----------------------------------------------------------#
 
     fig = plt.figure()                      #Set up a blank Figure
     ax = fig.add_subplot(1,1,1)             #Set up a blank axis in the Figure
     smap.plot()                             #Plot the Map
 
-    # Now, we may save the particular figure by formatting it per current index:
+    #Now, we may save the particular figure by formatting it per current index:
     plt.savefig(f'{work_dir}/map_from_fits_{index:03d}.png') # 03d -> 00{index}
     plt.show()
